99cc93e7a58b704876f4ab06d8239c5105986367
Nirbhay kaushik <kaushiknirbhay11@gmail.com>
2022-08-25 01:12:22 +0530

    Updates

---
+++ precache-manifest.14e40e86677d885c1f66d9303ddad1ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8e0b17fc64403ed41f7",
+    "url": "/Todo-App/static/css/main.ecd3e1d3.chunk.css"
+  },
+  {
+    "revision": "f8e0b17fc64403ed41f7",
+    "url": "/Todo-App/static/js/main.f8e0b17f.chunk.js"
+  },
+  {
+    "revision": "c7bf33405413e8c3ae07",
+    "url": "/Todo-App/static/js/1.c7bf3340.chunk.js"
+  },
+  {
+    "revision": "58db14a1c0e0e44501c3",
+    "url": "/Todo-App/static/js/runtime~main.58db14a1.js"
+  },
+  {
+    "revision": "6972cf6c22dfca0eed3958efe379188b",
+    "url": "/Todo-App/index.html"
+  }
+];
